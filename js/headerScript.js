653f2dce50d486111ee002cf9c745b9a94e6ff41
BeanGreen247 <mozdrent@gmail.com>
2020-04-05 09:47:20 +0200

    updated page style for a more modern look

---
+++ js/headerScript.js
@@ -0,0 +1,11 @@
+document.write(
+"<aside class='topNav nav-blue' id='topNav'>" +
+		"<div class='container navBar'>" +
+			"<a href='index.html'><h2>Home</h2> </a>" +
+			"<a href='mailto:mozdrent@gmail.com?Subject=Hi%20Thomas'><h2>E-Mail</h2> </a>" +
+			"<a target='_blank' href='cv/cv.pdf'><h2>CV</h2> </a>" +
+			"<a target='_blank'href='https://www.youtube.com/channel/UCQbA5Nc4oq6uMS2idDWsxkw'><h2>YouTube</h2> </a>"+
+      "<a target='_blank' href='https://github.com/BeanGreen247/beangreen247.github.io/tree/master/wallpaper'><h2>Wallpapers</h2> </a>"+
+		"</div>" +
+	"</aside>"
+);
---
+++ js/portfolio.js
@@ -0,0 +1,69 @@
+$(function() {
+
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+
+    $(function() {
+        $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    //#to-top button appears after scrolling
+    var fixed = false;
+    $(document).scroll(function() {
+        if ($(this).scrollTop() > 250) {
+            if (!fixed) {
+                fixed = true;
+                // $('#to-top').css({position:'fixed', display:'block'});
+                $('#to-top').show("slow", function() {
+                    $('#to-top').css({
+                        position: 'fixed',
+                        display: 'block'
+                    });
+                });
+            }
+        } else {
+            if (fixed) {
+                fixed = false;
+                $('#to-top').hide("slow", function() {
+                    $('#to-top').css({
+                        display: 'none'
+                    });
+                });
+            }
+        }
+    });
+
+	 $("#testButton").click(function(){
+
+		 $.get("https://beangreen247.github.io:3000/send",{to:"ayyy"},function(){
+
+		 });
+
+	 });
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-102268957-1', 'auto');
+  ga('send', 'pageview');
+
+});
