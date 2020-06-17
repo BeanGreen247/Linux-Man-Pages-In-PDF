ec231323d26de5e58d2e7cc076f8e46066e51998
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-06-17 14:21:12 +0200

    style change

--- js/headerScript.js
+++ js/headerScript.js
@@ -1,9 +1,7 @@
 document.write(
-"<aside class='topNav nav-blue' id='topNav'>" +
-		"<div class='container navBar'>" +
-			"<a title='Home' href='https://beangreen247.github.io/'><i class='fa fa-home fa-fw fa-2x'></i></a>" +
-			"<a title='Main channel' target='_blank'href='https://www.youtube.com/channel/UCQbA5Nc4oq6uMS2idDWsxkw'><i class='fa fa-youtube-play fa-fw fa-2x'></i></a>"+
-			"<a title='Music channel' target='_blank'href='https://www.youtube.com/channel/UC_Qo-mebrSPXU7EpkyJfJ0g'><i class='fa fa-youtube-play fa-fw fa-2x'></i></a>"+
-		"</div>" +
-	"</aside>"
+    "<div id='topNav' class='navbar'>"+
+        "<a title='Home' href='https://beangreen247.github.io/'>Home</i></a>" +
+        "<a title='Main channel' target='_blank'href='https://www.youtube.com/channel/UCQbA5Nc4oq6uMS2idDWsxkw'>Main YT channel</a>"+
+        "<a title='Music channel' target='_blank'href='https://www.youtube.com/channel/UC_Qo-mebrSPXU7EpkyJfJ0g'>Music YT channel</a>"+
+    "</div>"
 );
