74dd0efe7870923f4fca934aff9e6824f02f2525
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-06-21 08:01:10 +0200

    added icons

--- js/headerScript.js
+++ js/headerScript.js
@@ -1,7 +1,7 @@
 document.write(
     "<div id='topNav' class='navbar'>"+
-        "<a title='Home' href='https://beangreen247.github.io/'>Home</i></a>" +
-        "<a title='Main channel' target='_blank'href='https://www.youtube.com/channel/UCQbA5Nc4oq6uMS2idDWsxkw'>Main YT channel</a>"+
-        "<a title='Music channel' target='_blank'href='https://www.youtube.com/channel/UC_Qo-mebrSPXU7EpkyJfJ0g'>Music YT channel</a>"+
+        "<a title='Home' href='https://beangreen247.github.io/'><img height='25px' width='25px' src='icons/home.png'></img></a>" +
+        "<a title='Main channel' target='_blank'href='https://www.youtube.com/channel/UCQbA5Nc4oq6uMS2idDWsxkw'><img height='25px' width='25px' src='icons/yt.png'></img></a>"+
+        "<a title='Music channel' target='_blank'href='https://www.youtube.com/channel/UC_Qo-mebrSPXU7EpkyJfJ0g'><img height='25px' width='25px' src='icons/yt.png'></img></a>"+
     "</div>"
 );
